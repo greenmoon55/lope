b212e0b2181b5527fafcd0b607ee2aecf13b4a34
daisylv <daisylv2012@gmail.com>
2015-03-22 04:13:38 +0800

    still miss search and add column

---
+++ js/mydata.js
@@ -0,0 +1,22 @@
+var myplacedata = {
+	"imgsrc1":"img/hangzhou.jpg",
+	"imgsrc2":"img/suzhou.png",
+	"imgsrc3":"img/dongjing.png",
+	"imgsrc4":"img/shenhu.png",
+	"searchdata":[
+		"杭州",
+		"苏州",
+		"东京",
+		"神户"
+	],
+	"discription1":"杭州是著名的旅游城市，以风景秀丽著称。与苏州并称“苏杭”，素有“上有天堂下有苏杭”的美誉。",
+	"discription2":"苏州是中国首批24座国家历史文化名城之一，是吴文化的发祥地[3] 。苏州城始建于公元前514年，有2500多年历史。[1] 苏州园林是中国私家园林的代表。",
+	"discription3":"日本首都，位于本州岛关东平原南端。古时的东京是一个荒凉的渔村，最早的名称叫千代田。",
+	"discription4":"日本国际贸易港口城市，位于日本四大岛中最大的一个岛—本州岛的西南部，西枕六甲山，面向大阪湾。",
+	"icon":"img/flight.png"
+}
+
+ function getPlaceData() {
+    console.log(myplacedata["imgsrc1"]);
+    return myplacedata["imgsrc1"];
+}
