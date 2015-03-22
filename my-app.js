c3fe7c920966fd7d9b31ddd607dcb02415bc6cfa
greenmoon55 <greenmoon55@gmail.com>
2015-03-22 10:49:17 +0800

    sortable list is mostly functionally now!!!

--- my-app.js
+++ my-app.js
@@ -13,4 +13,22 @@ var mainView = myApp.addView('.view-main', {
 $$(document).on('pageInit', '.page[data-page="custom"]', function (e) {
   // Following code will be executed for page with data-page attribute equal to "about"
   window.onload = reloadData();
+  $$('.sortable').on('open', function() {
+    if ($$(source).hasClass('swipeout')) {
+      return false;
+    }
+    $$(".info-between").remove();
+  });
+  $$('.sortable').on('close', function(event) {
+    var source = event.target || event.srcElement;
+    if ($$(source).hasClass('swipeout')) {
+      return false;
+    }
+    $$(".info-between").remove();
+    var html="<div class='info-between' style='padding-left:40px;font-size:small'>耗时：30min ￥30RMB 折扣xxRMB</div>";
+    var lis = $$(this).find('li');
+    for (var i = 0; i < lis.length; i++) {
+      $$(html).insertAfter(lis[i]);
+    }
+  });
 })
