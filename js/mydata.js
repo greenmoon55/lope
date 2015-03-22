8e74a33e3ff6a3d44d72fa3c2613431353985372
daisylv <daisylv2012@gmail.com>
2015-03-22 09:31:12 +0800

    multple pages

--- js/mydata.js
+++ js/mydata.js
@@ -10,13 +10,43 @@ var myplacedata = {
 		"神户"
 	],
 	"discription1":"杭州是著名的旅游城市，以风景秀丽著称。与苏州并称“苏杭”，素有“上有天堂下有苏杭”的美誉。",
-	"discription2":"苏州是中国首批24座国家历史文化名城之一，是吴文化的发祥地[3] 。苏州城始建于公元前514年，有2500多年历史。[1] 苏州园林是中国私家园林的代表。",
+	"discription2":"苏州是中国首批24座国家历史文化名城之一，是吴文化的发祥地。苏州城始建于公元前514年，有2500多年历史。[1] 苏州园林是中国私家园林的代表。",
 	"discription3":"日本首都，位于本州岛关东平原南端。古时的东京是一个荒凉的渔村，最早的名称叫千代田。",
 	"discription4":"日本国际贸易港口城市，位于日本四大岛中最大的一个岛—本州岛的西南部，西枕六甲山，面向大阪湾。",
 	"icon":"img/flight.png"
 }
 
- function getPlaceData() {
-    console.log(myplacedata["imgsrc1"]);
-    return myplacedata["imgsrc1"];
+ function getPlaceData(key) {
+    //console.log(myplacedata["imgsrc1"]);
+    return myplacedata[key];
+}
+
+var itemsArr = new Array();
+var count = 0;
+itemsArr.push({
+	"imsrc":myplacedata["imgsrc1"],
+	"destination":myplacedata["searchdata"][0],
+	"description":myplacedata["description1"]
+});
+
+var itemHtml = "<div><li class='swipeout' style=''>"+
+                "<div class='swipeout-content item-content'>"+
+                  "<div class='item-media'><img src='"+itemsArr[count]["imsrc"]+"'></div>"+
+                  "<div class='item-inner'>"+itemsArr[count]["destination"]+"</div>"+
+                "</div>"+
+                "<div class='swipeout-actions-right '>"+
+                  "<a href='#' data-panel='right' class='action1 open-panel'>详情</a>"+
+                  "<a href='#' class='swipeout-delete'>删除</a>"+
+                "</div>"+
+              "</li>"+
+              "<p style='padding-left:40px;font-size:small'>"+itemsArr[count]["description"]+"</p>"+
+            "</div>";
+
+function reloadData() {
+	console.log(itemsArr.length - 1);
+	for (; count <= itemsArr.length - 1; count++) {
+		$('#itemListPanel').children().add(itemHtml);
+		console.log($('#itemListPanel').children());
+	};
+	count=0;
 }
